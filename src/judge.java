45b276ae8fcdc992dff46f915641ab9386452db7
Liwam <liwamtesfazion@gmail.com>
2021-04-29 02:12:26 -0700

    testing out

---
+++ src/judge.java
@@ -0,0 +1,9 @@
+public class judge {
+    public int findJudge (int N, int [][] trust){
+//        which takes two parameters, in order:
+//        ●N: int = number of people in the town
+//        ●trust: int [] [] = the trust array, in the form provided above and returns
+//        the ID of the judge or -1 if no judge is found.
+        return 0;
+    }
+}
